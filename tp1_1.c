b3d3083a37082f93b07cf3e53de1dc0e2da42e01
Agustin <m.a.g.p.2003@gmail.com>
2025-04-13 16:06:51 -0300

    Implementos punteros en el archivo tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -4,7 +4,19 @@
 
 int main()
 {   
-    printf("Hola Mundo");
+    int *puntero;
+    int num;
+
+    num = 5;
+    puntero = &num;
+
+    printf("Hola Mundo\n");
+
+    printf("El contenindo del puntero es: %i \n", *puntero);
+    printf("La direccion de memoria almacenada por el puntero es: %p\n",puntero);
+    printf("La direccion de memoria de la varaible se: %p\n", &num);
+    printf("La direccion de memoria del puntero es: %p\n", &puntero);
+    printf("El tamaño de momeria utilizado por la variable es: %lu\n", sizeof(num));
 
     return 0;
 }
